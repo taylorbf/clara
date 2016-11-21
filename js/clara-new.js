2ccf8d6b3cafcade4f35bf8210b44e14a1843afa
Ben Taylor <taylorbf@gmail.com>
2016-11-21 16:54:49 -0500

    updates

--- js/clara-new.js
+++ js/clara-new.js
@@ -30,12 +30,6 @@ function DTMeval(code) {
 
 function Melody(code) {
 	this.code = code
-	console.log(code)
-//	this.arr = DTMeval.call(dtm,this.code)
-/*	this.arr = function(str){
-		console.log(this)
-  	return eval(str);
-	}.call(dtm,code); */
 	this.arr = eval("with (dtm) {"+code+"}")
 	this.temp = this.arr
 	this.notes = this.arr.get()
@@ -107,23 +101,28 @@ melodies[0] = {
 	code: {},
 	engine: false
 }
-
-function evalCode() {
+function codeToData() {
+	data = {}
 	var code = codeeditor.value
   code = code.split("\n")
 	for (var i=0;i<code.length;i++) {
 		key = code[i].substring(0,code[i].indexOf(":"))
 		value = code[i].substring(code[i].indexOf(": ")+2)
-		melodies[0].code[key] = value
+		data[key] = value
 	}
-	console.log(melodies[0].code)
-	melodies[0].engine = new Melody(melodies[0].code.pitch)
-/*	try {
-		eval("with (dtm) { phrase = "+code+"}")
-		console.log(phrase)
-	} catch(e) {
-		console.log(e)
+	return data
+}
+
+function evalCode() {
+/*	var code = codeeditor.value
+  code = code.split("\n")
+	for (var i=0;i<code.length;i++) {
+		key = code[i].substring(0,code[i].indexOf(":"))
+		value = code[i].substring(code[i].indexOf(": ")+2)
+		melodies[0].code[key] = value
 	} */
+	melodies[0].code = codeToData()
+	melodies[0].engine = new Melody(melodies[0].code.pitch)
 }
 
 
@@ -133,16 +132,9 @@ function evalCode() {
 
 /* KEYBOARD VIS */
 
-viz = document.getElementById("viz")
-ctx = viz.getContext("2d")
-viz.width = 700
-viz.height = 100;
-viz.style.width = "700px";
-viz.style.height = "100px";
-
 codeeditor.addEventListener("keyup",function(e) {
 	if (e.which!=13) {
-	//	vizCode()
+		vizCode()
 	} else {
 		return false
 	}
@@ -165,50 +157,41 @@ function colorItBlue() {
 	}, 40)
 }
 
+
+var VizNotes = []
+
 function vizCode() {
-	code = codeeditor.value
+	data = codeToData()
 	var tempmelody = false
 	try {
-			tempmelody = false
-		tempmelody = eval(code)
-		tempmelody = tempmelody.temp
+		tempmelody = eval("with (dtm) {"+data.pitch+"}")
 	} catch(e) {
 		return
 	}
 	if (tempmelody) {
-		with (ctx) {
-			clearRect(0,0,viz.width,viz.height)
-			arr = tempmelody.get()
-			for (var i=0;i<arr.length;i++) {
-				if (typeof arr[i] === "function" ) {
-					var group = arr[i].get()
-				} else {
-					var group = [ arr[i] ]
-				}
-				for (var j=0;j<group.length;j++) {
-					note = group[j]
-					if (note>0) {
-						fillStyle = "#1ce"
-						var w = (viz.width / arr.length) / group.length
-						w = w > 40 ? 40 : w;
-						var x = i * (viz.width / arr.length)
-						var y = ((range - note)/range) * viz.height
-						x *= ( (viz.width-10) / viz.width )
-						x += 20
-						y *= ( (viz.height-10) / viz.height )
-						y += 20
-						beginPath()
-						ellipse(x,y,w/2,w/2,0,0,Math.PI*2)
-						fill()
-						closePath()
-					}
+		for (var i=0;i<VizNotes.length;i++) {
+			scene.remove(VizNotes[i])
+		}
+		VizNotes = []
+		arr = tempmelody.get()
+		for (var i=0;i<arr.length;i++) {
+			if (typeof arr[i] === "function" ) {
+				var group = arr[i].get()
+			} else {
+				var group = [ arr[i] ]
+			}
+			for (var j=0;j<group.length;j++) {
+				note = group[j]
+				if (note>=0) {
+
+					Graph.addNote(note)
+
 				}
 			}
 		}
 	}
 }
 
-vizCode()
 /*
 nx.onload = function() {
 	nx.colorize("fill","#222")
@@ -237,82 +220,46 @@ function updateKeyboard() {
 
 
 
-/* CYCLE VIZ */
 
-var Cycle = function(arr) {
-	this.data = arr
-	this.parent = document.getElementById("cycles")
-	this.canvas = document.createElement("canvas")
-	this.ctx = this.canvas.getContext("2d")
-	this.diameter = 75
-	this.canvas.height = this.diameter
-	this.canvas.width = this.diameter
-	this.canvas.style.width = this.diameter + "px"
-	this.canvas.style.height = this.diameter + "px"
-	this.canvas.className = "cycle"
-	this.parent.appendChild(this.canvas)
-	this.drawmelody = function() {
-		with(this.ctx) {
-			fillStyle = "#000"
-			fillRect(0,0,this.canvas.width,this.canvas.height)
-			beginPath()
-			fillStyle="#1ce"
-			for (var i=0;i<this.data.length;i++) {
-				var r = (this.diameter/2 - 15) * (this.data[i]/range) + 6
-				var angle = (Math.PI * 2) - ( i / this.data.length ) * Math.PI * 2
-				var loc = toCartesian(r,angle)
-				loc.x += (this.canvas.width/2)
-				loc.y += (this.canvas.height/2)
-				beginPath()
-					ellipse(loc.x,loc.y,1,1,0,Math.PI*2,false)
-					fill()
-				closePath()
-			}
-			strokeStyle = "#1ce"
-			fillStyle="#1ce"
-			lineWidth = 2
-			beginPath()
-				ellipse(this.canvas.width/2,this.canvas.height/2,6,6,0,Math.PI*2,false)
-				stroke()
-				fill()
-			closePath()
-		}
-	}
-	this.drawmelody()
-	this.drawprogress = function(n) {
-		// n is how many notes are left
-		with (this.ctx) {
-			strokeStyle = "#1ce"
-			lineWidth = 2
-			var progress = (this.data.length - n) / this.data.length
-			if (progress > 0) {
-				beginPath()
-					arc(this.canvas.width/2,this.canvas.height/2,this.diameter/2-2,0,progress*Math.PI*2,false)
-					stroke()
-				closePath()
-			}
-		}
-	}
-	this.tempactions = []
-	this.kill = function() {
-		this.tempactions.push( this._kill )
-	}
-	this._kill = function() {
-		this.parent.removeChild(this.canvas)
-	}
-	this.update = function(arr) {
-		this.data = arr
-		this.drawmelody()
-		for (var i=0;i<this.tempactions.length;i++) {
-			this.tempactions[i]()
-		}
-		this.tempactions = []
+
+/* Graphing */
+
+
+var Graph = {
+	addNote: function() {
+
+		var geometry = new THREE.Geometry();
+
+		xloc = VizNotes.length;
+		geometry.vertices.push({
+			x: xloc,
+			y: note-30,
+			z: 0
+		});
+		geometry.vertices.push({
+			x: xloc + 2,
+			y: note-30,
+			z: 0
+		});
+
+		var line = new THREE.Line( geometry, new THREE.LineBasicMaterial( { color: 0xffffff, opacity: 1, linewidth: 2, linecap: "round"  } ) );
+		scene.add( line );
+		VizNotes.push( line )
+	//	camera.position.z = VizNotes.length
 	}
 }
 
-cycles = []
-//var testmelody = dtm.line(20).repeat(4)
-//cycles.push[ new Cycle(testmelody.get()) ]
+
+
+
+
+vizCode()
+
+
+
+
+
+
 
 
 
@@ -333,19 +280,4 @@ function indexToNote(index) {
 	console.log(modaldegree)
 	var midinote = base + octave*12 + modaldegree
 	return midinote
-}
-
-var toPolar = function(x,y) {
-  var r = Math.sqrt(x*x + y*y);
-  var theta = Math.atan2(y,x);
-  if (theta < 0.) {
-    theta = theta + (2 * Math.PI);
-  }
-  return {radius: r, angle: theta};
-}
-
-var toCartesian = function(radius, angle){
-  var cos = Math.cos(angle);
-  var sin = Math.sin(angle);
-  return {x: radius*cos, y: radius*sin*-1};
 }
